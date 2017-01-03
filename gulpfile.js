f483be67faceb4a0232580ec1b7aa0721602b4a6
Camille Sébastien Niessen <camille@overstappen.nl>
2017-01-03 16:20:40 +0100

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+/* GULPFILE
+------------------------------------------------------- */
+'use strict';
+
+const gulp = require('gulp');
+const concat = require('gulp-concat');	// Concatonate files
+const uglify = require('gulp-uglify');	// Minify files
+const rename = require('gulp-rename');	// Rename files
+const sass = require('gulp-sass');	// Sass compiler
+const maps = require('gulp-sourcemaps');  // Create sourcemaps to map the original source
+
+/* CONCAT ALL JS FILES
+------------------------------------------------------- */
+gulp.task('concatJS', function(){
+	return gulp.src([
+		  'src/public/js/components/*.js',
+			'src/public/js/main.js'])
+	.pipe(maps.init())
+	.pipe(concat('scripts.js'))
+	.pipe(maps.write('./'))
+	.pipe(gulp.dest('src/public/dist/js'));
+});
+
+/* MINIFY ALL JS FILES
+------------------------------------------------------- */
+gulp.task('minifyJS', ['concatJS'], function(){
+	return gulp.src("src/public/dist/js/scripts.js")
+	.pipe(uglify())
+	.pipe(rename('scripts.min.js'))
+	.pipe(gulp.dest('src/public/dist/js'));
+});
+
+/* COMPILE SASS
+------------------------------------------------------- */
+gulp.task('compileSass', function(){
+	return gulp.src('src/public/style/main.scss')
+	.pipe(maps.init())
+	.pipe(sass())
+	.pipe(maps.write('./'))
+	.pipe(gulp.dest('src/public/dist/style'))
+});
+
+/* BUILD TASK
+------------------------------------------------------- */
+gulp.task('build', ['minifyJS', 'compileSass']);
+
+/* DEFAULT TASK
+------------------------------------------------------- */
+gulp.task('default', ['build']);
--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 /* DEPENDENCIES INLADEN
---------------------------------------------------------- */
+------------------------------------------------------- */
 var express = require('express');
 var app = express();
 
 /* SETUP
---------------------------------------------------------- */
+------------------------------------------------------- */
 
 // Heroku can set the port for the application
 var port = process.env.PORT || 8080;
 
 // EJS als view engine
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
 // Express looks for assets in public folder
 app.use(express.static(__dirname + '/public'));
 
 /* ROUTERS
---------------------------------------------------------- */
+------------------------------------------------------- */
 
 app.get('/', function(req, res) {
     res.render('index');
@@ -24,7 +24,7 @@ app.get('/', function(req, res) {
 
 
 /* START APPLICATION
---------------------------------------------------------- */
+------------------------------------------------------- */
 app.listen(port, function() {
-    console.log('The Heroku APP is running on: http://localhost:' + port);
+    console.log('APP IS RUNNING ON: http://localhost:' + port);
 });
