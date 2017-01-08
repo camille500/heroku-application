515f7d20f1c2e848576ef2469057500d047d678f
Camille Sébastien Niessen <camille@overstappen.nl>
2017-01-09 00:11:03 +0100

    Postgres deleted

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const app = express();
 
 const bodyParser = require('body-parser');
 const path = require('path');
-const pg = require('pg');
-
 
 /* SETUP
 ------------------------------------------------------- */
@@ -34,18 +32,6 @@ app.use('/account', accountRouters);
 
 app.get('/', function(req, res) {
     res.render('index');
-});
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
 });
 
 app.enable('verbose errors');
