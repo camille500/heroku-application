5daf70a5cd2a59e824929a6c11f1d41d512ef0f4
Camille Sébastien Niessen <camille@overstappen.nl>
2017-01-11 17:06:02 +0100

    Connect to Postges test

--- server.js
+++ server.js
@@ -35,16 +35,15 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
     });
-  });
 });
 
 app.enable('verbose errors');
